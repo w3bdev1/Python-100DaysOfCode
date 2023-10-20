dcc9d64d75aecd414cb3b704bb1078d3bcb74948
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-20 09:26:11 +0530

    Day 1: Band name generator

---
+++ D1 - Bandname Generator/main.py
@@ -0,0 +1,8 @@
+print("*** WELCOME TO BANDNAME GENERATOR ***")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+
+print("---------------------------------------------------")
+print("Your customised band name is - " + city + " " + pet)
+print("---------------------------------------------------")
+print("Keep rocking \m/")
