5eab4a6970c351a301760fc769f0045ab3059110
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-21 07:53:26 +0530

    Life in weeks (and days and months till 90 years)

---
+++ D2 - Tip Calculator/life-in-weeks.py
@@ -0,0 +1,8 @@
+curr_age = int(input('Your current age: '))
+
+years_left = 90 - curr_age
+days_left = years_left * 365
+weeks_left = years_left * 52
+months_left = years_left * 12
+
+print(f"You have {days_left} days, {weeks_left} weeks, {months_left} months left till age of 90.")
