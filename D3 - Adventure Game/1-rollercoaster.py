68734dfc2e9648a32e2cc242b8bac61a25f5afc5
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-24 08:59:30 +0530

    Roller coaster

---
+++ D3 - Adventure Game/1-rollercoaster.py
@@ -0,0 +1,7 @@
+print("Welcome to the Roller Coaster!")
+height = int(input("What is your height (in cm)? "))
+
+if height >= 120:
+    print('You are allowed to ride')
+else:
+    print('You are not allowed')
