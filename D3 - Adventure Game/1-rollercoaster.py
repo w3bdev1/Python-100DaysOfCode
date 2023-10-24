6e11e81045d46bf198c138f8e1f41553b9a95b8f
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-24 09:44:30 +0530

    Roller coaster charge for photo

--- D3 - Adventure Game/1-rollercoaster.py
+++ D3 - Adventure Game/1-rollercoaster.py
@@ -3,13 +3,24 @@ height = int(input("What is your height (in cm)? "))
 
 if height >= 120:
     print('You are allowed to ride')
+    bill = 0
 
     age = int(input("What is your age (in years)? "))
     if age >= 18:
-        print('Pay ₹100')
+        print('Adult ticket price ₹100')
+        bill = 100
     elif age >= 12:
-        print('Pay ₹70')
+        print('Youth ticket price ₹70')
+        bill = 70
     else:
-        print('Pay ₹50')
+        print('Child ticket price ₹50')
+        bill = 50
+    
+    want_photo = input('Do you want your photo taken? (Y or N): ')
+    if want_photo == 'Y':
+        print('Extra charge for photo: ₹20')
+        bill += 20
+    
+    print(f"Total bill: {bill}")
 else:
     print('You are not allowed')
