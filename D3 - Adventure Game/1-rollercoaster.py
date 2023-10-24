432f86b7881bb16e438b7c52c77c0ef45a460f37
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-24 09:00:18 +0530

    Roller coaster age

--- D3 - Adventure Game/1-rollercoaster.py
+++ D3 - Adventure Game/1-rollercoaster.py
@@ -3,5 +3,11 @@ height = int(input("What is your height (in cm)? "))
 
 if height >= 120:
     print('You are allowed to ride')
+
+    age = int(input("What is your age (in years)? "))
+    if age >= 18:
+        print('Pay ₹100')
+    else:
+        print('Pay ₹50')
 else:
     print('You are not allowed')
