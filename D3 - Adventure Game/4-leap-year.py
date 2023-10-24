97006692f62320f62cadc94e7cc0295d608e70c8
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-24 09:44:15 +0530

    Leap year checker

---
+++ D3 - Adventure Game/4-leap-year.py
@@ -0,0 +1,13 @@
+print('Leap Year Checker')
+year = int(input('Give me your choice of year: '))
+
+if year%4 == 0:
+    if year%100 == 0:
+        if year%400 == 0:
+            print('It is a leap year')
+        else:
+            print('It is not a leap year')
+    else:
+        print('It is a leap year')
+else:
+    print('It is not a leap year')
