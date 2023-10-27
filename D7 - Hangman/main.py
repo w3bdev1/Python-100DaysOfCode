bb17b6705a4368c486019848cae67d9b443e39ee
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-27 14:25:09 +0530

    Show dashed word with guessed character position

--- D7 - Hangman/main.py
+++ D7 - Hangman/main.py
@@ -5,11 +5,15 @@ words = ['salutation', 'reverence', 'pulvarize']
 # choose random word
 chosen_word = random.choice(words)
 
+# dashed chars
+dashed_chars = ['_' for _ in range(len(chosen_word))]
+print(dashed_chars)
+
 # ask user to guess a letter
 guess = input('Guess a letter: ').lower()
 
-for letter in chosen_word:
+for id, letter in enumerate(chosen_word):
     if letter == guess:
-        print('Present')
-    else:
-        print('Absent')
+        dashed_chars[id] = letter
+
+print(dashed_chars)
