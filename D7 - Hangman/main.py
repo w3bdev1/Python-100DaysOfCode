67a6e4168f5ce091bca56bbb3296dfb3ff3641eb
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-27 14:16:29 +0530

    Check a guess

--- D7 - Hangman/main.py
+++ D7 - Hangman/main.py
@@ -5,4 +5,11 @@ words = ['salutation', 'reverence', 'pulvarize']
 # choose random word
 chosen_word = random.choice(words)
 
-print(chosen_word)
+# ask user to guess a letter
+guess = input('Guess a letter: ').lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print('Present')
+    else:
+        print('Absent')
