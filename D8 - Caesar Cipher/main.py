123364f996caf92d4dad998132d55413fb38adad
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-28 13:16:45 +0530

    Combined encrypt and decrypt into one function

--- D8 - Caesar Cipher/main.py
+++ D8 - Caesar Cipher/main.py
@@ -6,33 +6,50 @@ directive = input("Type 'e' to encrypt and 'd' to decrypt: ").strip().lower()
 msg = input("Type your message: ").strip().lower()
 shift = int(input("Shift number: ").strip())
 
-def encrypt(text, shift_amount):
+def caesar(text, shift_amount, directive):
     new_message = ""
     for c in text:
         if c == ' ':
             new_message += ' '
         else:
-            new_alphabet_index = alphabets.index(c) + shift_amount
-            if new_alphabet_index > len(alphabets):
-                new_alphabet_index = new_alphabet_index - len(alphabets)
-            new_message += alphabets[new_alphabet_index]
-    print(f"Encrypted text: {new_message}")
+            if directive == 'e':
+                new_alphabet_index = alphabets.index(c) + shift_amount
+                if new_alphabet_index > len(alphabets):
+                    new_alphabet_index = new_alphabet_index - len(alphabets)
+            elif directive == 'd':
+                new_alphabet_index = alphabets.index(c) - shift_amount
+                if new_alphabet_index < 0:
+                    new_alphabet_index = len(alphabets) + new_alphabet_index
+            else:
+                print('Invalid direction!')
+                return
 
-def decrypt(text, shift_amount):
-    new_message = ""
-    for c in text:
-        if c == ' ':
-            new_message += ' '
-        else:
-            new_alphabet_index = alphabets.index(c) - shift_amount
-            if new_alphabet_index < 0:
-                new_alphabet_index = len(alphabets) + new_alphabet_index
             new_message += alphabets[new_alphabet_index]
-    print(f"Decrypted text: {new_message}")
+    print(f"Cipher text: {new_message}")
 
-if directive == 'e':
-    encrypt(text=msg, shift_amount=shift)
-elif directive == 'd':
-    decrypt(text=msg, shift_amount=shift)
-else:
-    print("Invalid direction!")
+caesar(text=msg, directive=directive, shift_amount=shift)
+
+# def encrypt(text, shift_amount):
+#     new_message = ""
+#     for c in text:
+#         if c == ' ':
+#             new_message += ' '
+#         else:
+#             new_alphabet_index = alphabets.index(c) + shift_amount
+#             if new_alphabet_index > len(alphabets):
+#                 new_alphabet_index = new_alphabet_index - len(alphabets)
+#             new_message += alphabets[new_alphabet_index]
+#     print(f"Encrypted text: {new_message}")
+# 
+# def decrypt(text, shift_amount):
+#     new_message = ""
+#     for c in text:
+#         if c == ' ':
+#             new_message += ' '
+#         else:
+#             new_alphabet_index = alphabets.index(c) - shift_amount
+#             if new_alphabet_index < 0:
+#                 new_alphabet_index = len(alphabets) + new_alphabet_index
+#             new_message += alphabets[new_alphabet_index]
+#     print(f"Decrypted text: {new_message}")
+
