af88d0b1a70ebdb28afaff64866988b7b9b53ca5
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-31 18:56:25 +0530

    Early return if one of the param is empty

--- D10 - Calculator/1-name-tltle.py
+++ D10 - Calculator/1-name-tltle.py
@@ -1,4 +1,7 @@
 def format_name(first_name, last_name):
+    if first_name == "" or last_name == "":
+        return "Invalid inputs"
     return first_name.title() + ' ' + last_name.title()
 
 print(format_name("sudEEP", "BISWAS"))
+print(format_name("sudEEP", ""))
