4ed16fb76d116d68a9072010afd260a00dbb8e5b
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-31 20:18:00 +0530

    Recursion to use start calculation

--- D10 - Calculator/main.py
+++ D10 - Calculator/main.py
@@ -17,28 +17,34 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("First number: "))
+def calculator():
+    num1 = int(input("First number: "))
 
-for operator in operations:
-    print(operator)
+    for operator in operations:
+        print(operator)
 
-end_of_calculation = False
-while not end_of_calculation:
+    end_of_calculation = False
+    while not end_of_calculation:
 
-    operator = input("Choose an operator: ")
+        operator = input("Choose an operator: ")
 
-    num2 = int(input("Next number: "))
+        num2 = int(input("Next number: "))
 
-    operation_function = operations[operator]
-    ans = operation_function(num1, num2)
-    print(f"{num1} {operator} {num2} = {ans}")
-    
+        operation_function = operations[operator]
+        ans = operation_function(num1, num2)
+        print(f"{num1} {operator} {num2} = {ans}")
+        
 
-    again = input(f"Continue calculating with {ans}? [y/n] ").lower()
-    if again == 'y':
-        num1 = ans
-    elif again == 'n':
-        end_of_calculation = True
-    else:
-        print("Invalid input")
-        break
+        again = input(f"Continue calculating with {ans}? [y/n/q] ").lower()
+        if again == 'y':
+            num1 = ans
+        elif again == 'n':
+            end_of_calculation = True
+            calculator()
+        elif again == 'q':
+            end_of_calculation = True
+        else:
+            print("Invalid input!")
+            end_of_calculation = True
+
+calculator()
