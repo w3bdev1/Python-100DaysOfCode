0bb05e91133da4a8369ac5a7202abff677037f63
Sudeep Biswas <w3bdev@protonmail.com>
2023-10-31 19:54:48 +0530

    Chain 2nd operation

--- D10 - Calculator/main.py
+++ D10 - Calculator/main.py
@@ -16,3 +16,26 @@ operations = {
     "-": subtract,
     "/": divide
 }
+
+num1 = int(input("First number: "))
+
+for operator in operations:
+    print(operator)
+
+operator = input("Choose one of the above operators: ")
+
+num2 = int(input("Second number: "))
+
+operation_function = operations[operator]
+ans = operation_function(num1, num2)
+
+print(f"{num1} {operator} {num2} = {ans}")
+
+operator = input("Choose one of the above operators: ")
+
+num3 = int(input("Third number: "))
+
+operation_function = operations[operator]
+new_ans = operation_function(ans, num3)
+
+print(f"{ans} {operator} {num3} = {new_ans}")
