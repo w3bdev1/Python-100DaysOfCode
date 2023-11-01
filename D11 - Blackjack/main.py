3aef502044a26b9f4ffde337c5c4c49420ced555
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-01 23:02:39 +0530

    Functions to check value in hhand

--- D11 - Blackjack/main.py
+++ D11 - Blackjack/main.py
@@ -22,3 +22,14 @@ player_cards = random.choices(list(cards.keys()), k=2)
 
 # Choose 1 card for computer
 computer_cards = random.choices(list(cards.keys()), k=1)
+
+# Check value in hand
+def calculate_hand(cards_in_hand):
+    total = 0
+    for card in cards_in_hand:
+        total += cards[card]
+    return total
+def calculate_player_hand():
+    return calculate_hand(player_cards)
+def calculate_computer_hand():
+    return calculate_hand(computer_cards)
