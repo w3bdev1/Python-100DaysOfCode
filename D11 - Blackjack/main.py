38c8450e3c36349d9f65c80e1fdb62c195f9ab8e
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-01 23:19:32 +0530

    Functions to check result

--- D11 - Blackjack/main.py
+++ D11 - Blackjack/main.py
@@ -19,9 +19,11 @@ cards = {
 
 # Choose 2 card for player
 player_cards = random.choices(list(cards.keys()), k=2)
+print(f"Your hand: {player_cards}")
 
 # Choose 1 card for computer
 computer_cards = random.choices(list(cards.keys()), k=1)
+print(f"Opponent's hand: {computer_cards}")
 
 # Check value in hand
 def calculate_hand(cards_in_hand):
@@ -33,3 +35,18 @@ def calculate_player_hand():
     return calculate_hand(player_cards)
 def calculate_computer_hand():
     return calculate_hand(computer_cards)
+
+# Check result
+def result():
+    player_hand = calculate_player_hand()
+    computer_hand = calculate_computer_hand()
+    if player_hand == computer_hand:
+        print("Draw!")
+    elif player_hand > 21 or computer_hand == 21:
+        print("You lose!")
+    elif computer_hand > 21 or player_hand == 21:
+        print("You win!")
+    elif player_hand > computer_hand:
+        print("You win!")
+    else:
+        print("You lose!")
