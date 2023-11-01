7d102209e410c870e053c91c5fdfa800a6d2c281
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-02 00:23:25 +0530

    Restart the game

--- D11 - Blackjack/main.py
+++ D11 - Blackjack/main.py
@@ -1,4 +1,5 @@
 import random
+import os
 import ascii_art
 
 cards = {
@@ -18,32 +19,17 @@ cards = {
     'K': 10,
 }
 
-# Initialize
-print(ascii_art.logo)
-
-# Choose 2 card for player
-player_cards = random.choices(list(cards.keys()), k=2)
-print(f"Your hand: {player_cards}")
-
-# Choose 1 card for computer
-computer_cards = random.choices(list(cards.keys()), k=1)
-print(f"Opponent's hand: {computer_cards}")
-
 # Check value in hand
 def calculate_hand(cards_in_hand):
     total = 0
     for card in cards_in_hand:
         total += cards[card]
     return total
-def calculate_player_hand():
-    return calculate_hand(player_cards)
-def calculate_computer_hand():
-    return calculate_hand(computer_cards)
 
 # Check result
-def result():
-    player_hand = calculate_player_hand()
-    computer_hand = calculate_computer_hand()
+def result(player_cards, computer_cards):
+    player_hand = calculate_hand(player_cards)
+    computer_hand = calculate_hand(computer_cards)
     print(f"Result: Your hand - {player_hand}, Opponent's hand - {computer_hand}")
     if player_hand > 21:
         print("You lose!")
@@ -58,26 +44,41 @@ def result():
     else:
         print("You lose!")
 
-# New card for player
-end_of_pick = False
-while not end_of_pick:
-    picks_another = input('Do you want to pick another card? [y/n] ').lower()
-    if picks_another == 'y':
-        player_cards.append(random.choice(list(cards.keys())))
-        print(f"Your hand: {player_cards}")
-    else:
-        end_of_pick = True
-        break
-    
-    if calculate_player_hand() >= 21:
-        end_of_pick = True
+def play():
+    # Initialize
+    print(ascii_art.logo)
 
-# New card for computer
-is_choosing = [True, False]
-if calculate_player_hand() <= 21 and random.choice(is_choosing):
-    while calculate_computer_hand() < 21:
-        computer_cards.append(random.choice(list(cards.keys())))
+    # Choose 2 card for player
+    player_cards = random.choices(list(cards.keys()), k=2)
+    print(f"Your hand: {player_cards}")
+
+    # Choose 1 card for computer
+    computer_cards = random.choices(list(cards.keys()), k=1)
     print(f"Opponent's hand: {computer_cards}")
 
-result()
-    
+    # New card for player
+    end_of_pick = False
+    while not end_of_pick:
+        picks_another = input('Do you want to pick another card? [y/n] ').lower()
+        if picks_another == 'y':
+            player_cards.append(random.choice(list(cards.keys())))
+            print(f"Your hand: {player_cards}")
+        else:
+            end_of_pick = True
+            break
+        
+        if calculate_hand(player_cards) >= 21:
+            end_of_pick = True
+
+    # New card for computer
+    is_choosing = [True, False]
+    if calculate_hand(player_cards) <= 21 and random.choice(is_choosing):
+        while calculate_hand(computer_cards) < 21:
+            computer_cards.append(random.choice(list(cards.keys())))
+        print(f"Opponent's hand: {computer_cards}")
+
+    result(player_cards, computer_cards)
+        
+while input('Do you want a game of BlackJack? [y/n] ').lower() == 'y':
+    os.system('clear')
+    play()
