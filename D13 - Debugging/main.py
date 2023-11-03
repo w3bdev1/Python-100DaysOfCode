e9de3f9f596f10a2a32022e49385d1ca4f244660
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-03 18:26:01 +0530

    Fixed the bug

--- D13 - Debugging/main.py
+++ D13 - Debugging/main.py
@@ -17,7 +17,7 @@
 year = int(input("What's your year of birth?"))
 if year > 1980 and year < 1994:
   print("You are a millenial.")
-elif year > 1994:
+elif year >= 1994:
   print("You are a Gen Z.")
 
 # # Fix the Errors
