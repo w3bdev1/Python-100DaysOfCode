fd55a7f1e3e0eba337ab57e195a520742bc68304
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-03 19:07:25 +0530

    Fixed the bug

--- D13 - Debugging/odd-even.py
+++ D13 - Debugging/odd-even.py
@@ -1,6 +1,6 @@
 number = int(input("Which number do you want to check?"))
 
-if number % 2 = 0:
+if number % 2 == 0:
     print("This is an even nunber.")
 else:
     print("This is an odd nunber.")
