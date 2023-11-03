3b08b70a640b1ed332dc1237f87f8cbc00d3637b
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-03 18:16:53 +0530

    Fixed the bug

--- D13 - Debugging/main.py
+++ D13 - Debugging/main.py
@@ -10,8 +10,7 @@
 # Reproduce the Bug
 from random import randint
 dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(1, 6)
-dice_num = 6
+dice_num = randint(0, 5)
 print(dice_imgs[dice_num])
 
 # # Play Computer
