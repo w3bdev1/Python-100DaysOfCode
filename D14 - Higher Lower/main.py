5533465695a7f416f5a8ede587485b3fbcb02863
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-04 21:12:13 +0530

    Final touches

--- D14 - Higher Lower/main.py
+++ D14 - Higher Lower/main.py
@@ -3,42 +3,34 @@ import os
 data = __import__('game-data').data
 from art import logo, vs
 
-# Pick 2 random person
+# Initialization
+print(logo)
 persons = random.choices(data, k=2)
-print(f"A: {persons[0]}")
-print(f"B: {persons[1]}")
+
+def user_won(choice_id):
+    other_index = 1 if choice_index == 0 else 0
+    choice_follower_count = persons[choice_index]['follower_count']
+    other_follower_count = persons[other_index]['follower_count']
+    return choice_follower_count > other_follower_count
+
+def print_rivalry():
+    print(f"Compare A: {persons[0]['name']} -- {persons[0]['description']} from {persons[0]['country']}")
+    print(vs)
+    print(f"Compare B: {persons[1]['name']} -- {persons[1]['description']} from {persons[1]['country']}")
 
 user_score = 0
 end_of_game = False
 while not end_of_game:
-    # Print out their details
-    print(f"""
-    Compare A: {persons[0]['name']} -- {persons[0]['description']} from {persons[0]['country']}
-    {vs}
-    Against B: {persons[1]['name']} -- {persons[1]['description']} from {persons[1]['country']}
-    """
-    )
-
-    # User chooses
+    print_rivalry()
     user_choice = input("Who has more followers? Type 'A' or 'B': ")
-
     if user_choice == 'A':
         choice_index = 0
     else:
         choice_index = 1
 
-    # If wrong game ends
-
-    # If right, 2nd choice become 1st and pick another random choice
-
-    def user_won():
-        other_index = 1 if choice_index == 0 else 0
-        choice_follower_count = persons[choice_index]['follower_count']
-        other_follower_count = persons[other_index]['follower_count']
-        return choice_follower_count > other_follower_count
-
-    if user_won():
+    if user_won(choice_index):
         os.system('clear')
+        print(logo)
         print("You're correct!")
         user_score +=1
         print(f"SCORE: {user_score}")
