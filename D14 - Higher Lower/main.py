2c5036ba68890e1ad4f8b17ad91ba528890a8627
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-04 21:01:08 +0530

    Game loop added

--- D14 - Higher Lower/main.py
+++ D14 - Higher Lower/main.py
@@ -7,35 +7,38 @@ persons = random.choices(data, k=2)
 print(f"A: {persons[0]}")
 print(f"B: {persons[1]}")
 
-# Print out their details
-print(f"""
-Compare A: {persons[0]['name']} -- {persons[0]['description']} from {persons[0]['country']}
-{vs}
-Against A: {persons[1]['name']} -- {persons[1]['description']} from {persons[1]['country']}
-"""
-)
+end_of_game = False
+while not end_of_game:
+    # Print out their details
+    print(f"""
+    Compare A: {persons[0]['name']} -- {persons[0]['description']} from {persons[0]['country']}
+    {vs}
+    Against B: {persons[1]['name']} -- {persons[1]['description']} from {persons[1]['country']}
+    """
+    )
 
-# User chooses
-user_choice = input("Who has more followers? Type 'A' or 'B': ")
+    # User chooses
+    user_choice = input("Who has more followers? Type 'A' or 'B': ")
 
-if user_choice == 'A':
-    choice_index = 0
-else:
-    choice_index = 1
+    if user_choice == 'A':
+        choice_index = 0
+    else:
+        choice_index = 1
 
-# If wrong game ends
+    # If wrong game ends
 
-# If right, 2nd choice become 1st and pick another random choice
+    # If right, 2nd choice become 1st and pick another random choice
 
-def user_won():
-    other_index = 1 if choice_index == 0 else 0
-    choice_follower_count = persons[choice_index]['follower_count']
-    other_follower_count = persons[other_index]['follower_count']
-    return choice_follower_count > other_follower_count
+    def user_won():
+        other_index = 1 if choice_index == 0 else 0
+        choice_follower_count = persons[choice_index]['follower_count']
+        other_follower_count = persons[other_index]['follower_count']
+        return choice_follower_count > other_follower_count
 
-if user_won():
-    print("You win")
-else:
-    print("You lose!")
-
-# Repeat
+    if user_won():
+        print("You win")
+        persons.pop(0)
+        persons.append(random.choice(data))
+    else:
+        print("You lose!")
+        end_of_game = True
