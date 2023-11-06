ebfbbf53f9e07f2baad695d7e24239e48678bd3a
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-06 23:52:54 +0530

    Check if resources are sufficient

--- D15 - Coffee Machine/main.py
+++ D15 - Coffee Machine/main.py
@@ -1,3 +1,5 @@
+from menu import MENU
+
 flavours: list[str] = ['espresso', 'latte', 'cappuccino']
 
 resources = {
@@ -16,7 +18,19 @@ def report():
     print(f"Money: ${money}")
 
 
+def is_sufficient(flavour):
+    for ingredient, required_amount in MENU[flavour]["ingredients"].items():
+        if required_amount > resources[ingredient]:
+            print(f"Sorry there is not enough {ingredient}.")
+            return False
+    return True
+
+
 user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
-if user_input == report:
+if user_input == "report":
     report()
+elif user_input == "off":
+    pass
+elif (user_input in flavours) and is_sufficient(user_input):
+    print(f"Creating {user_input}")
