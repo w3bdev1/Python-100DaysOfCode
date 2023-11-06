07f851b55a9b3d56532ea56613333da25b78f4b4
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-06 23:35:27 +0530

    Print report

--- D15 - Coffee Machine/main.py
+++ D15 - Coffee Machine/main.py
@@ -5,3 +5,15 @@ resources = {
     "milk": 200,
     "coffee": 100,
 }
+
+money = 0
+
+
+def report():
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${money}")
+
+
+report()
