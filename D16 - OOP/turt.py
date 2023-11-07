7f23007986993632af9234d75f18c39023da7bf2
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-07 18:31:45 +0530

    Playing with turtle module

---
+++ D16 - OOP/turt.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+turt = Turtle()
+print(turt)
+turt.shape("turtle")
+turt.color("DarkOrange")
+turt.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvwidth)
+my_screen.exitonclick()
+
