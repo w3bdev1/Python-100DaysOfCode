b9ba110e79d75b1c5879ea909714b2b054f45af0
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-08 18:56:32 +0530

    Question bank added

--- D17 - True False Quiz/main.py
+++ D17 - True False Quiz/main.py
@@ -1,1 +1,7 @@
-# Quiz app
+from questions import question_data
+from question_model import Question
+
+question_bank = [Question(q["text"], q["answer"]) for q in question_data]
+
+q1 = question_bank[0]
+print(q1.text, q1.answer)
--- D17 - True False Quiz/question_model.py
+++ D17 - True False Quiz/question_model.py
@@ -2,7 +2,3 @@ class Question:
     def __init__(self, text, answer):
         self.text = text
         self.answer = answer
-
-
-q = Question("2+2=4", "True")
-print(q.text, q.answer)
