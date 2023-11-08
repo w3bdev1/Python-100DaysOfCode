01d5702edae957c13bb3b1d8e7b33ca59b27c8bd
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-08 19:14:40 +0530

    Question Brain asking next question

--- D17 - True False Quiz/main.py
+++ D17 - True False Quiz/main.py
@@ -1,7 +1,9 @@
 from questions import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = [Question(q["text"], q["answer"]) for q in question_data]
 
-q1 = question_bank[0]
-print(q1.text, q1.answer)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
+
