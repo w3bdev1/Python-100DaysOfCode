ffe8628e036fbdac7444e2568d1832a8edd1751a
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-08 19:30:28 +0530

    Check user answer

--- D17 - True False Quiz/quiz_brain.py
+++ D17 - True False Quiz/quiz_brain.py
@@ -5,8 +5,15 @@ class QuizBrain:
 
     def next_question(self):
         q = self.question_list[self.question_number]
-        input(f"Q{self.question_number + 1}. {q.text} (True/False)? ")
+        user_answer = input(f"Q{self.question_number + 1}. {q.text} (True/False)? ")
+        self.check_answer(user_answer, correct_answer=q.answer)
         self.question_number += 1
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("Correct!")
+        else:
+            print(f"That's wrong. It was {correct_answer}.")
 
     def still_has_question(self):
         return self.question_number < len(self.question_list)
