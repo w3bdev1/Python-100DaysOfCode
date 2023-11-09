24a3527c9d5522be4e22d07282120f417d57478c
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-09 21:30:04 +0530

    draw each color to check

--- D18 - Turtle Graphics/main.py
+++ D18 - Turtle Graphics/main.py
@@ -1,6 +1,22 @@
 import colorgram
+from turtle import Turtle, Screen
 
 colors = colorgram.extract("image.jpg", 30)
 colors = [tuple(color.rgb) for color in colors]
+turt = Turtle()
+turt.speed("fastest")
+screen = Screen()
+screen.colormode(255)
 
 print(colors)
+
+x = 0
+for c in colors:
+    turt.color(c)
+    turt.begin_fill()
+    turt.circle(10)
+    turt.end_fill()
+    x += 10
+    turt.setx(x)
+
+screen.exitonclick()
