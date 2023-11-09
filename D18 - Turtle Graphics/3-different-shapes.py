1f55deaa9cbef4b307353acaa67b37e1a5e3ed4b
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-09 19:53:42 +0530

    Random stroke color

--- D18 - Turtle Graphics/3-different-shapes.py
+++ D18 - Turtle Graphics/3-different-shapes.py
@@ -1,18 +1,28 @@
 from turtle import Turtle, Screen
+from random import randint
 
 turt = Turtle()
+turt.pensize(5)
+screen = Screen()
 
 
 def draw_polygon(number_of_sides: int):
     internal_angle = 360 / number_of_sides
-
     for _ in range(number_of_sides):
+        randomize_color()
         turt.fd(100)
         turt.right(internal_angle)
+
+
+def randomize_color():
+    screen.colormode(255)
+    r = randint(0, 255)
+    g = randint(0, 255)
+    b = randint(0, 255)
+    turt.pencolor((r, g, b))
 
 
 for side in range(3, 11):
     draw_polygon(side)
 
-screen = Screen()
 screen.exitonclick()
