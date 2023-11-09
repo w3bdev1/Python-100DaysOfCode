37a131697e87b96754c09c12b399f4c4e421e2b7
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-09 21:32:01 +0530

    Copied the colors excluding first 3 shades of white

--- D18 - Turtle Graphics/main.py
+++ D18 - Turtle Graphics/main.py
@@ -1,14 +1,15 @@
-import colorgram
 from turtle import Turtle, Screen
 
-colors = colorgram.extract("image.jpg", 30)
-colors = [tuple(color.rgb) for color in colors]
+colors = [(210, 234, 244), (18, 112, 172), (187, 16, 63), (232, 144, 74), (243, 218, 77), (171, 44, 105),
+          (228, 125, 158), (40, 50, 119), (225, 57, 108), (114, 170, 209), (228, 77, 60), (63, 169, 79),
+          (124, 190, 146), (127, 78, 48), (16, 139, 63), (10, 173, 207), (209, 154, 9), (151, 209, 219), (18, 52, 97),
+          (23, 96, 47), (117, 43, 35), (252, 228, 0), (69, 76, 41), (5, 78, 51), (240, 163, 186), (153, 210, 195),
+          (236, 170, 161)]
+
 turt = Turtle()
 turt.speed("fastest")
 screen = Screen()
 screen.colormode(255)
-
-print(colors)
 
 x = 0
 for c in colors:
