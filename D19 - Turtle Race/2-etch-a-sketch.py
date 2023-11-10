dfa9aac3f6167ab5583dcd55e882d11180d48c0e
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-10 09:54:12 +0530

    Can use arrow keys too

--- D19 - Turtle Race/2-etch-a-sketch.py
+++ D19 - Turtle Race/2-etch-a-sketch.py
@@ -5,9 +5,13 @@ screen = Screen()
 
 screen.listen()
 screen.onkeypress(key="w", fun=lambda: turt.forward(10))
+screen.onkeypress(key="Up", fun=lambda: turt.forward(10))
 screen.onkeypress(key="s", fun=lambda: turt.backward(10))
+screen.onkeypress(key="Down", fun=lambda: turt.backward(10))
 screen.onkeypress(key="a", fun=lambda: turt.left(10))
+screen.onkeypress(key="Left", fun=lambda: turt.left(10))
 screen.onkeypress(key="d", fun=lambda: turt.right(10))
+screen.onkeypress(key="Right", fun=lambda: turt.right(10))
 screen.onkeypress(key="c", fun=lambda: turt.reset())
 screen.onkeypress(key="q", fun=lambda: screen.bye())
 
