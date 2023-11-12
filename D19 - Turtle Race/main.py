ffb857d831af4628c0229870d89cb2ef1277d710
Sudeep Biswas <w3bdev@protonmail.com>
2023-11-12 18:59:01 +0530

    Game is on...

--- D19 - Turtle Race/main.py
+++ D19 - Turtle Race/main.py
@@ -1,13 +1,16 @@
 from turtle import Turtle, Screen
+from random import randint
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 PADDING = 20
 INITIAL_X = -(screen.window_width() / 2) + PADDING
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Choose a color: ")
-print(user_bet)
 
 colors = ["violet", "blue", "green", "yellow", "orange", "red"]
+user_bet = screen.textinput(title="Make your bet",
+                            prompt=f"Which turtle will win the race? ({'/'.join(c for c in colors)}) ").lower()
+turtles = []
 y = -125
 
 for color in colors:
@@ -15,6 +18,24 @@ for color in colors:
     t.color(color)
     t.penup()
     t.goto(x=INITIAL_X, y=y)
+    turtles.append(t)
     y += 50
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in turtles:
+        if turtle.xcor() > (250 - 20):
+            is_race_on = False
+            winner_color = turtle.pencolor()
+            print(f"{winner_color.title()} turtle is the winner!")
+            if winner_color == user_bet:
+                print("You've won!")
+            else:
+                print("You've lost!")
+        else:
+            random_distance = randint(0, 10)
+            turtle.forward(random_distance)
 
 screen.exitonclick()
